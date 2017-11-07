854bd1df236ae2759c8e4a1c2e7c678be7516d5a
dcaban <davidjcaban@gmail.com>
2017-11-07 14:31:19 -0500

    testing new way of recieving score

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -7,8 +7,42 @@ module.exports = function (app) {
     });
 
     app.post("/api/friends", function(req, res) {
+
+        var newFriend = {
+            name: "",
+            photo: "",
+            friendDifference: 1000
+        };
+
+               console.log("im running");
+                   var newFriend = req.body;
+                   console.log(friendData);
+                   var newName = newFriend.name;
+                   var newScore = req.body.q1;
+
+                   console.log("work damnit " + newScore);
+                   // for (var i = 0; i < 9; i++) {
+                   //     var newScores = parseInt(newFriend[i].scores[i]);
+                   //     console.log(newScores)
+                  // }
+
+
+           var difference = 0;
+
+
+                   // for (var i = 0; i < (friendData.length); i++) {
+                   //
+                   //     console.log(friendData[i].name);
+                   //
+                   //       for (var j=0; j< friendData[i].scores[j]; j++){
+                   //           console.log("testio"+ friendData[i].scores[j]);
+                   //           // console.log(friendsData[i].scores[x]);
+                   //
+                   // }
+                   // }
+
         friendData.push(req.body);
 
-
+        res.json(newFriend)
     });
 };
